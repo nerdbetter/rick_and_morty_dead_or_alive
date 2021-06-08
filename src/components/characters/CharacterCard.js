58113b4afd5870cf60433487cca9b63ab0ad5a5b
Jason Logan <jasontlogan@gmail.com>
2021-06-08 13:18:57 -0500

    played around with styles

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -1,24 +1,38 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 
-export const CharacterCard = ({ name, gender, species, location, status, origin, image }) => {
+export const CharacterCard = ({
+  name,
+  gender,
+  species,
+  location,
+  status,
+  origin,
+  image,
+}) => {
+  console.log(name, gender, species, location, status, origin, image);
+  const deadoralive = status === "Dead" ? "red" : "#97ce4c"
 
-    console.log(name, gender, species, location, status, origin, image)
- 
   const dispatch = useDispatch();
 
-  const onNoteClick = () => {
-      
-  }
+  const onNoteClick = () => {};
 
   return (
-    <div className='characterCard hover'>
-      <h1 className='characterCard_headers'>{name}</h1>
-      <h2 className='characterCard_headers'>Gender: {gender}</h2>
-      <h2 className='characterCard_headers'>Species: {species}</h2>
-      <h2 className='characterCard_headers'>Location: {location.name}</h2>
-      <h2 className='characterCard_headers'>status: {status}</h2>
-      <h2 className='characterCard_headers'>Origin: {origin.name}</h2>      
+    <div className="characterCard">
+      <div className="characterCard_headers_wrapper">
+        <h1 className="characterCard_headers">{name}</h1>
+        <h2 className="characterCard_headers">Gender: {gender}</h2>
+        <h2 className="characterCard_headers">Species: {species}</h2>
+        <h2 className="characterCard_headers">Location: {location.name}</h2>
+        <h2 className="characterCard_headers">Origin: {origin.name}</h2>
+      </div>
+      <div
+        className="characterCard portrait"
+        style={{backgroundImage: `url(${image})`}}
+      >
+        <span style={{color:`${deadoralive}`}}>status: {status}</span>
+        <button className="note_button">notes</button>
+      </div>
     </div>
   );
 };
